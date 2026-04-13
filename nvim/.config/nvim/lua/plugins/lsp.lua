922a8715ab24eca4c5931b87595e1ab00a50160c
andriiprokhorenko <prokhor.andrew@gmail.com>
2026-04-13 09:43:26 -0500

    Add dart, bash, json, postgres LSPs

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      { "mason-org/mason.nvim" },
-      { "neovim/nvim-lspconfig" },
-    },
-    opts = {
-      ensure_installed = { "lua_ls", "ts_ls" },
-      automatic_enable = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim" },
+			{ "neovim/nvim-lspconfig" },
+		},
+		opts = {
+			ensure_installed = { "bashls", "lua_ls", "ts_ls", "dartls", "jsonls", "postgres_lsp" },
+			automatic_enable = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
 }
